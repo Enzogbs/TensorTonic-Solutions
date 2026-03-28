34a50e4239be79cdf23d1855449fdf29458f6644
Enzo Soares <enzog2302@gmail.com>
2026-03-28 15:48:41 -0300

    Add solution for make-diagonal

---
+++ make-diagonal/make-diagonal.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def make_diagonal(v):
+    """
+    Returns: (n, n) NumPy array with v on the main diagonal
+    """
+    v = np.array(v)
+    matrix = np.eye(v.shape[0])
+
+    return matrix * v
