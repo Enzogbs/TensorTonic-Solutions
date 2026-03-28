d4997c8110920e8d6e4f72f1e6d01d74fea402de
Enzo Soares <enzog2302@gmail.com>
2026-03-28 13:12:23 -0300

    Update solution for precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -4,11 +4,10 @@ def precision_recall_at_k(recommended, relevant, k):
     """
     Compute precision@k and recall@k for a recommendation list.
     """
-    if k > len(recommended):
-        return None
+    # if k > len(recommended):
+    #     return None
+        
     recommended, relevant = np.array(recommended[:k]), np.array(relevant)
-
-    
     
     precision = np.intersect1d(recommended, relevant).size / k
     recall = np.intersect1d(recommended, relevant).size / relevant.size
