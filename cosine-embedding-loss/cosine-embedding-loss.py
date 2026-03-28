2a9716b3e68223de2c3346f6ceaa858010086fdc
Enzo Soares <enzog2302@gmail.com>
2026-03-28 13:41:41 -0300

    Add solution for cosine-embedding-loss

---
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -0,0 +1,20 @@
+import numpy as np
+import numpy.linalg as LA
+
+def cosine_embedding_loss(x1, x2, label, margin):
+    x1, x2 = np.array(x1), np.array(x2)
+    
+    norm1, norm2 = LA.norm(x1), LA.norm(x2)
+    if norm1 == 0 or norm2 == 0:
+        return None
+    
+    cos = np.dot(x1 / norm1, x2 / norm2)
+    
+    if label == 1:
+        return 1 - cos
+    elif label == -1:
+        return max(0, cos - margin)
+    else:
+        return None
+
+    
